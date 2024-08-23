469590396e7acd3b100400aa0b49d0b979612e36
CarlosEdu20 <eduardocarlos3134@gmail.com>
2024-08-23 17:48:54 -0300

    Decodificador de texto

---
+++ app.js
@@ -0,0 +1,55 @@
+let textInput = document.querySelector("#input-text");
+let outInput = document.querySelector("#conteudo__saida__inicial");
+
+function criptografar() {
+    let texto = textInput.value;
+
+    if(validarTexto(texto)){
+        let textCript = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+        outInput.innerHTML = `<h2 id="output-text">${textCript}</h2><button class="btn-copiar" onclick="copiar()">Copiar</button>`;
+    }
+}
+
+function descriptografar() {
+    let texto = textInput.value;
+
+    if(validarTexto(texto)){
+        let textDescript = texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+        outInput.innerHTML = `<h2 id="output-text">${textDescript}</h2><button class="btn-copiar" onclick="copiar()">Copiar</button>`;
+
+    }
+}
+
+function copiar() {
+    let textoCopiar = document.getElementById("output-text");
+    let selection = window.getSelection();
+    let range = document.createRange();
+    range.selectNodeContents(textoCopiar);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    alert('Texto copiado');
+}
+
+function validarTexto(texto){
+    let regex = /^[a-z]+$/
+
+    if (regex.test(texto)){
+        return true
+    }
+    else{
+        alert("Texto inválido, Digite apenas letras minúsculas e sem acento")
+        return false
+    }
+}
+
